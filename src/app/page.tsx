2345b5db052730dd55f245381bdff3468b47b942
Antoniel <antonielzeferino0@gmail.com>
2025-01-23 08:56:07 -0300

    add background image responsivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,43 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import homeBg from "../../public/images/home-bg.png";
+import homeBg from "../../public/images/mobile-bg.png";
+import desktopBg from "../../public/images/desktop-bg.png";
 
 function Home() {
+  const [backgroundImage, setBackgroundImage] = useState(homeBg);
+
+  useEffect(() => {
+    const updateBackgroundImage = () => {
+      setBackgroundImage(window.innerWidth > 768 ? desktopBg : homeBg);
+    };
+
+    // Define a imagem inicial
+    updateBackgroundImage();
+
+    // Adiciona o listener para o resize
+    window.addEventListener("resize", updateBackgroundImage);
+
+    // Remove o listener ao desmontar o componente
+    return () => window.removeEventListener("resize", updateBackgroundImage);
+  }, []);
+
   return (
     <div className="min-h-screen w-full overflow-hidden flex flex-col md:flex-row items-center justify-center">
       {/* Imagem de fundo - no mobile ocupa toda a tela, e em telas grandes ocupa metade */}
-      <div className="w-full h-screen max-w-3xl md:max-w-[425px] lg:max-w-lg relative">
+      <div className="w-full h-screen max-w-3xl md:max-w-[425px] lg:max-w-full relative">
         <Image
-          src={homeBg}
+          src={backgroundImage}
           alt="Home Background"
           layout="fill"
           objectFit="cover"
           className="w-full h-full"
         />
-        
+
         {/* Texto sobre a imagem no mobile */}
-        <div className="md:hidden absolute inset-0 flex items-start mt-32 justify-center text-center p-8 md:p-0">
+        <div className="md:hidden absolute inset-0 flex items-start mt-[12vh] justify-center text-center p-8 md:p-0">
           <div className="text-gray-700">
             <h1 className="text-4xl font-serif text-pink-600 mb-4">Doce Essência</h1>
             <p className="text-lg mb-6">Beleza, cosméticos e cuidados pessoais</p>
@@ -30,7 +51,7 @@ function Home() {
       </div>
 
       {/* Em telas grandes, o texto e a imagem ficam lado a lado */}
-      <main className="hidden md:block p-8 text-center md:text-left max-w-lg md:w-1/2">
+      <main className="hidden md:block p-8 text-center md:text-left max-w-lg md:w-full">
         <h1 className="text-4xl font-serif text-pink-600 mb-4">Doce Essência</h1>
         <p className="text-gray-700 font-light text-lg mb-6">
           Beleza, cosméticos e cuidados pessoais
